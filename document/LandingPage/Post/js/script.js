17fe650a4cfee70ef2f968b91fe5ca3501fadb1a
Toxa1704 <Toxa1704@outlook.com>
2024-12-09 12:44:52 +0200

    main

--- document/LandingPage/Post/js/script.js
+++ document/LandingPage/Post/js/script.js
@@ -1,1 +1,30 @@
-console.log('ok');
+const burgerMenu = document.querySelector('.header-burger-menu');
+const headerMenu = document.querySelector('.header-menu');
+const headerMenuList = document.querySelectorAll('.header-menu-list>a');
+const checkOut = document.querySelector('.check-out');
+const sendingBtns = document.querySelector('.sending-btns')
+const natural = document.querySelector('.natural');
+const business = document.querySelector('.business');
+console.log(headerMenuList);
+
+
+burgerMenu.addEventListener('click',() =>{
+    headerMenu.style.display = 'block'
+    console.log('ok');
+})
+headerMenuList.forEach(link => {
+  link.addEventListener('click', (event) => {
+    headerMenu.style.display = 'none'
+console.log('link ok');
+  });
+});
+
+checkOut.addEventListener('click', () => {
+  sendingBtns.style.display = 'block'
+})
+natural.addEventListener('click', () => {
+  sendingBtns.style.display = 'none'
+})
+business.addEventListener('click', () => {
+  sendingBtns.style.display = 'none'
+})
